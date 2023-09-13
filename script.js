72ac37deadec0b0ecba765bde431dfc906ec8a30
Adarsh Gupta <114940577+adarshg3371@users.noreply.github.com>
2023-09-13 11:32:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,63 @@
-function calculateMinCost() {
-  //your code here
-  
-  
-  
-}  
+// JavaScript
+function minHeapify(arr, i, heapSize) {
+    const left = 2 * i + 1;
+    const right = 2 * i + 2;
+    let smallest = i;
+
+    if (left < heapSize && arr[left] < arr[i]) {
+        smallest = left;
+    }
+
+    if (right < heapSize && arr[right] < arr[smallest]) {
+        smallest = right;
+    }
+
+    if (smallest !== i) {
+        [arr[i], arr[smallest]] = [arr[smallest], arr[i]];
+        minHeapify(arr, smallest, heapSize);
+    }
+}
+
+function buildMinHeap(arr) {
+    const n = arr.length;
+    for (let i = Math.floor(n / 2); i >= 0; i--) {
+        minHeapify(arr, i, n);
+    }
+}
+
+function extractMin(arr, heapSize) {
+    if (heapSize <= 0) return -1;
+    if (heapSize === 1) {
+        heapSize--;
+        return arr.pop();
+    }
+
+    const root = arr[0];
+    arr[0] = arr[heapSize - 1];
+    heapSize--;
+    minHeapify(arr, 0, heapSize);
+
+    return root;
+}
+
+function calculateMinimumCost() {
+    const inputElement = document.getElementById("inputRopes");
+    const resultElement = document.getElementById("result");
+    const ropeLengths = inputElement.value.split(",").map(Number);
+
+    const heapSize = ropeLengths.length;
+    buildMinHeap(ropeLengths);
+
+    let totalCost = 0;
+
+    while (heapSize > 1) {
+        const min1 = extractMin(ropeLengths, heapSize);
+        const min2 = extractMin(ropeLengths, heapSize);
+        const newRope = min1 + min2;
+        totalCost += newRope;
+        ropeLengths.push(newRope);
+        heapSize++;
+    }
+
+    resultElement.textContent = `Minimum Cost: ${totalCost}`;
+}
